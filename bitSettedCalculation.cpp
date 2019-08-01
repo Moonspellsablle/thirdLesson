86358bfde5e43e05299127fa0f38c49204b6a36b
Vlad Adaskalitsa <Moonspellsablle@MacBook-Air-Vlad.local>
2019-08-01 23:27:08 +0300

    Fixed some logical moments. 1) Use now fixed types, such as int64_t. 2) Remove {if} construction.

--- bitSettedCalculation.cpp
+++ bitSettedCalculation.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
+#include <stdint.h>
 
 int main() {
-  unsigned long int userDataVariable;
+  uint64_t userDataVariable;
   std::cout << "Please, enter number (I will calculate count of '1' bits): " << std::endl;
   std::cin >> userDataVariable;
-  short int count = 0;
+  uint16_t count = 0;
   for(size_t shift = 0; userDataVariable >> shift; ++shift) {
-    if ((userDataVariable >> shift) & 1) {
-      ++count;
-    }
+    count += ((userDataVariable >> shift) & 1);
   }
   std::cout << count << std::endl;
 }

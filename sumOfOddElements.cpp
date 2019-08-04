ed2460bfd94da8e2c9a82471e08783614bd2c6e8
“Vladyslav” <moonspellsablle@gmail.com>
2019-08-04 15:02:09 +0300

    Added cstdint lib. Changed cout, fixed some not logical moments

--- sumOfOddElements.cpp
+++ sumOfOddElements.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
+#include <cstdint>
 
 int main() {
-  std::cout << "Please, enter a count of input elements: ";
-  short int countOfElements;
+  std::cout << "Please, enter a count of input elements [1..50], each of element must be in range [-60..90] (I will calculate summ of odd elements): ";
+  uint16_t countOfElements;
   std::cin >> countOfElements;
 
-  int sumOfOddElements = 0;
+  int32_t sumOfOddElements = 0;
   for (size_t i = 0; i < countOfElements; ++i) {
     std::cout << "Please, enter an element: ";
-    short int element;
+    int16_t element;
     std::cin >> element;
-    if (!((element % 2) == 0)) {
+    if ((element % 2) != 0) {
       sumOfOddElements += element;
     }
   }

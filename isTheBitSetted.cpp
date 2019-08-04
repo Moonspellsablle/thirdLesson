e8368fc4388e366d3337218bfeb6b5a8c13eb42a
“Vladyslav” <moonspellsablle@gmail.com>
2019-08-04 21:13:07 +0300

    Changed loggic for bool expression

--- isTheBitSetted.cpp
+++ isTheBitSetted.cpp
@@ -6,7 +6,7 @@ int main() {
   uint16_t userDataShift;
   std::cout << "Please, enter number and bit shift, separated by a space: (I will find, if the bit is setted): ";
   std::cin >> userDataVariable >> userDataShift;
-  bool isBitSetted = (((userDataVariable >> (userDataShift - 1)) & 1) && (userDataShift > 0));
+  bool isBitSetted = ((userDataShift > 0) && ((userDataVariable >> (userDataShift - 1)) & 1));
   std::cout << (isBitSetted ? "Yes\n" : "No\n");
 
   return 0;
